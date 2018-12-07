973000101412ce96f6cc81861bda65d298299623
Tin Cya <32213532+nghichcode@users.noreply.github.com>
2018-12-07 07:03:23 +0000

    OK index

---
+++ index.js
@@ -0,0 +1,43 @@
+var http = require('http');
+var url= require('url');
+var querystring = require('querystring');
+
+var login = require('./login');
+var logout = require('./logout');
+var picking = require('./picking');
+
+http.createServer(function (req, res) {
+
+	// POST data
+	const FORM_URLENCODED = 'application/x-www-form-urlencoded';
+    if(req.headers['content-type'] === FORM_URLENCODED && req.method === "POST") {
+    	var body = '';
+        req.on('data', function(chunk) {body += chunk.toString();});
+        req.on('end', function() {response(querystring.parse(body));});
+    }
+
+    function response(params) {
+    	var resContent = {};
+    	var urlParams = url.parse(req.url, true);
+
+		res.writeHead(200,{'Content-Type':'text/json'});
+		var correctToken =  params.username && params.password && params.client_id && params.client_secret && params.grant_type;
+		var correctRevoke =  req.headers['authorization'];
+		var correctPicking = req.headers['authorization'] && params.whCode && params.customerCode && params.pickingNo;
+
+		if (urlParams.pathname == "/oauth/token") {
+			resContent = login.parse(params);
+		} else if (urlParams.pathname == "/oauth/revoke" && correctRevoke) {
+			resContent = logout.parse(req.headers['authorization']);
+		} else if (urlParams.pathname == "/api/picking/sacnSerial.json" && correctPicking) {
+			resContent = picking.parse(req.headers['authorization'], params);
+		} else {
+			resContent = {status: "wrongParams",statusText:"Wrong Parameters"};
+		};
+		res.write(JSON.stringify(resContent));
+		res.end();
+    }
+
+
+}).listen(8080);
+// console.log(body);
